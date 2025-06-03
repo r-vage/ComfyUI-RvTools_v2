8b7b6d0e20a966b70bdec51dbd7854e9f57742ec
rvage0815 <skentler@protonmail.com>
2025-06-03 23:49:54 +0200

    added smaller checkpoint loader (v2 pipe)

---
+++ py/RvCheckpointLoader_Small_v2_Pipe.py
@@ -0,0 +1,67 @@
+import comfy
+import comfy.sd
+import torch
+import folder_paths
+
+from ..core import CATEGORY
+
+MAX_RESOLUTION = 32768
+
+class RvCheckpointLoader_Small_v2_Pipe:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "ckpt_name": (folder_paths.get_filename_list("checkpoints"),),
+                "vae_name": (["Baked VAE"] + folder_paths.get_filename_list("vae"),),
+                "stop_at_clip_layer": ("INT", {"default": -1, "min": -24, "max": -1, "step": 1},),
+            },
+        }
+
+    CATEGORY = CATEGORY.MAIN.value + CATEGORY.CHECKPOINT.value
+
+    RETURN_TYPES = ("pipe",)
+    FUNCTION = "execute"
+
+    def execute(self, ckpt_name, vae_name, stop_at_clip_layer):
+        ckpt_path = folder_paths.get_full_path("checkpoints", ckpt_name)
+        output_vae = (vae_name == "Baked VAE")
+
+        loaded_ckpt = comfy.sd.load_checkpoint_guess_config(ckpt_path, output_vae=output_vae, output_clip=True, embedding_directory=folder_paths.get_folder_paths("embeddings"),)
+
+        vae_path = ""
+
+        if vae_name == "Baked VAE":
+            loaded_vae = loaded_ckpt[:3][2]
+        else:
+            vae_path = folder_paths.get_full_path("vae", vae_name)
+            loaded_vae = comfy.sd.VAE(sd=comfy.utils.load_torch_file(vae_path))
+
+        loaded_clip = loaded_ckpt[:3][1].clone()
+        loaded_clip.clip_layer(stop_at_clip_layer)
+
+        #model, clip, vae, batch_size, modelname, vae_name = pipe
+
+        rlist = []
+        rlist.append(loaded_ckpt[:3][0])
+        rlist.append(loaded_clip)
+        rlist.append(loaded_vae)
+        rlist.append(int(1))              #batch_size (for compatibility)
+        rlist.append(str(ckpt_name))      #str(ckpt_path)) #model_name
+        rlist.append(str(vae_name))       #vae_name (no path)
+
+        return (rlist,)
+
+NODE_NAME = 'Checkpoint Loader Small v2 (Pipe) [RvTools]'
+NODE_DESC = 'Checkpoint Loader Small v2 (Pipe)'
+
+NODE_CLASS_MAPPINGS = {
+   NODE_NAME: RvCheckpointLoader_Small_v2_Pipe
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    NODE_NAME: NODE_DESC
+}
