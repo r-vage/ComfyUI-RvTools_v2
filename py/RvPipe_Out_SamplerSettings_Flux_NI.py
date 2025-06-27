dd8e3817032edcd245a89ce192ac5d5a3d5fefee
rvage0815 <skentler@protonmail.com>
2025-06-27 18:03:23 +0200

    added sampler settings flux with more values for noise injection

---
+++ py/RvPipe_Out_SamplerSettings_Flux_NI.py
@@ -0,0 +1,33 @@
+from .anytype import *
+
+from ..core import CATEGORY
+
+class RvPipe_Out_SamplerSettings_Flux_NI:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {"required": {"pipe": ("pipe",),}}
+
+    CATEGORY = CATEGORY.MAIN.value + CATEGORY.PIPE.value
+    RETURN_TYPES = ("pipe",  any,       any,        "INT",   "FLOAT", "FLOAT", "FLOAT", "FLOAT", "FLOAT")
+    RETURN_NAMES = ("pipe", "sampler", "scheduler", "steps", "cfg",   "flux_guidance", "denoise","sigmas_denoise","noise_strength")
+
+    FUNCTION = "execute"
+
+    def execute(self, pipe=None, ):
+        sampler, scheduler, steps, cfg, flux_guidance, denoise, sigmas_denoise, noise_strength  = pipe
+        
+        return pipe, sampler, scheduler, steps, cfg, flux_guidance, denoise, sigmas_denoise, noise_strength
+
+NODE_NAME = 'Pipe Out Sampler Settings (Flux) [RvTools]'
+NODE_DESC = 'Pipe Out Sampler Settings (Flux)'
+
+NODE_CLASS_MAPPINGS = {
+   NODE_NAME: RvPipe_Out_SamplerSettings_Flux_NI
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    NODE_NAME: NODE_DESC
+}
